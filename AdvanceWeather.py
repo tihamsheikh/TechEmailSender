a4a0535a7280c0163685c97cab7dc1f41135e5ee
Fardin Sheikh Tiham <tihamsheikhone@gmail.com>
2025-03-27 22:50:27 +0600

    Add files via upload

--- AdvanceWeather.py
+++ AdvanceWeather.py
@@ -3,24 +3,26 @@
 # api open weather
 
 # key hash- do not remove!!!!
-# hTaChIdMrBfTafjf 43uh934 fnbc948fhhiu(*Y
-# aHdOhSfA9Y3I48(*GB(*YNR*(W89798g&pG 78iu
-# oIgDj98D*(&HG e 89w4987q437ghcfch(* f
-# uS2E37fEdh7A7G87 789 f43hyc8h34897 nF
-# hajfh uawehM uisadhniw fiuhfsud fhhia
+# hTaChIdMrBfTafjf 43uh934 f
+# aHdOhSfA9Y3I48(*GB(*YNR*(W
+# oIgDj98D*(&HG e 89w4987
+# uS2E37fEdh7A7G87 789 f4
+# hajfh uawehM uisadhniw
 
+# email pkgs
 import smtplib, requests, schedule, time
 import KEYS
 from google import genai
-from email.mime.multipart import MIMEMultipart as mime_multipart
-from email.mime.text import MIMEText as mime_text
-
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+# GUI pkgs
 import tkinter as tk
 import ttkbootstrap as ttk
 
+state = True
+
 # weather info acquiring (1st operation)
 def weather_info(place):
-
     api_key = KEYS.open_weather_key
 
     weather_call_url = f"https://api.openweathermap.org/data/2.5/weather?appid={api_key}&q={place}"
@@ -56,7 +58,6 @@ def summarize_weather_info(place):
 
 # email sending section (3rd operation)
 def email_section(username, password, client, place):
-
     port = 587
     host = "smtp.gmail.com"
 
@@ -81,26 +82,25 @@ def email_section(username, password, client, place):
     for info in content:
         if info == "":
             continue
-        # print(template_list[count])
-        # print(info)
-        main_content += template_list[count]
-        main_content += info
+        # print(template_list[count], "\n",info)
+        main_content += template_list[count]    # adding html
+        main_content += info                    # adding content
         count += 1
     main_content += template_list[-1]
 
     # email formating (end)
 
-    message = mime_multipart()  # email template
+    message = MIMEMultipart()  # email template
     message["To"] = client
     message["From"] = username
     message["Subject"] = "Weather News!!!!"
-    message.attach(mime_text(main_content, "html"))
+    message.attach(MIMEText(main_content, "html"))
 
     server.send_message(message)
     server.quit()
     print("Successfully\nEmail sent")
 
-# sending call
+# mail sending part (operation 4)
 def mailing():
     # use get() of the tkinter var on deployment
     username = "e.zero.bd.i@gmail.com"
@@ -109,16 +109,15 @@ def mailing():
     place = "Dhaka"
 
     email_section(username, app_password, client, place)
+# scheduled mail sending part (operation 4 [optional])
+def mailing_start(interval):
+    schedule.every(interval).minute.do(mailing) # change to hour on deployment
 
-def initiation(interval):
-
-    schedule.every(interval).minute.do(mailing)
-    countdown = 0
-    while True:
+    while state:
         schedule.run_pending()
-        print(f"Sending mail in {countdown}")
-        time.sleep(20)
-        countdown += 1
+        print("Running.....")
+        time.sleep(20)  # change to 1800 on hourly rate
+
 
 # GUI start
 app = ttk.Window()
@@ -148,32 +147,94 @@ label.pack(padx=2, pady=5, ipadx=2, ipady=2)
 
 
 # widget for username, password, client, place
+frame_username = ttk.Frame(master=app)
+frame_password= ttk.Frame(master=app)
+frame_client = ttk.Frame(master=app)
+frame_place = ttk.Frame(master=app)
+frame_interval = ttk.Frame(master=app)
+frame_buttons = ttk.Frame(master=app)
+
+frame_username.pack(pady=4)
+frame_password.pack(pady=4)
+frame_client.pack(pady=4)
+frame_place.pack(pady=4)
+frame_interval.pack(pady=4)
+frame_buttons.pack(pady=10)
 
 # username widget
 username_label = ttk.Label(
-    master=app,
+    master=frame_username,
     text="Your Email ",
     font=font
 )
-username_entry = ttk.Entry(master=app, font=font, textvariable=username_var)
+username_entry = ttk.Entry(master=frame_username, font=font, textvariable=username_var)
 
-username_label.pack()
-username_entry.pack()
+username_label.pack(side="left", ipadx=5)
+username_entry.pack(ipadx=10)
 
 # password widget
 password_label = ttk.Label(
-    master=app,
-    text="Email Password",
+    master=frame_password,
+    text="Password",
     font=font
 )
-password_entry = ttk.Entry(master=app, font=font, textvariable=password_var)
+password_entry = ttk.Entry(master=frame_password, font=font, textvariable=password_var)
 
-password_label.pack()
-password_entry.pack()
+password_label.pack(side="left", ipadx=2)
+password_entry.pack(ipadx=10)
 
 # client widget
+client_label = ttk.Label(
+    master=frame_client,
+    text="Client Email",
+    font=font
+)
+client_entry = ttk.Entry(master=frame_client, font=font, textvariable=client_var)
+
+client_label.pack(side="left", ipadx=2)
+client_entry.pack(ipadx=10)
 
 # place widget
+place_label = ttk.Label(
+    master=frame_place,
+    text="Destination",
+    font=font
+)
+place_entry = ttk.Entry(master=frame_place, font=font, textvariable=place_var)
+
+place_label.pack(side="left", ipadx=2)
+place_entry.pack(ipadx=10)
+
+# Interval selection
+
+interval_button = ttk.Radiobutton(
+    master=frame_interval
+)
+interval_button.pack()
+
+# submit and reset
+
+submit_btn = ttk.Button(
+    master=frame_buttons,
+    text="Submit"
+)
+
+reset_btn = ttk.Button(
+    master=frame_buttons,
+    text="Reset"
+)
+
+submit_btn.pack(side="left", ipadx=10,  padx=15)
+reset_btn.pack(side="left", ipadx=15, padx=15)
+
+
+
+
+
+
+
+
+
 
 
 app.mainloop()
